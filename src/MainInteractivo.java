044e8520fb0379923b1570774fd9cd32fa9025f6
Tomas <tomasgf13@gmail.com>
2026-05-02 16:50:52 -0700

    arreglo de la documentacion javadoc y mejores comentarios

--- src/MainInteractivo.java
+++ src/MainInteractivo.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class main_interactivo {
+public class Main_interactivo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         /**
@@ -19,7 +19,9 @@ public class main_interactivo {
                 break;
             }
 
-            // 1. Selección de Moneda
+            /**
+             * Selección de Moneda
+             */
             System.out.println("Seleccione su moneda: 1) $100  2) $500  3) $1000");
             int tipoMoneda = sc.nextInt();
             Moneda monedaEntregada = null;
@@ -28,24 +30,30 @@ public class main_interactivo {
             else if (tipoMoneda == 2) monedaEntregada = new Moneda500();
             else if (tipoMoneda == 3) monedaEntregada = new Moneda1000();
 
-            // 2. Selección de Producto (Usando tu Enumeración)
+            /**
+             * Selección de Producto (Usando TipoPodructo))
+             */
             System.out.println("Seleccione producto: 1) CocaCola  2) Sprite  3) Fanta  4) Snickers  5) Super8");
             int seleccionProd = sc.nextInt();
 
             // Aquí debes convertir el número a tu constante de la Enumeración
-            Enumeracion TipoProducto = null;
+            // El tipo de dato es "TipoProducto", no "Enumeracion"
+            TipoProducto productoElegido = null;
+
             switch (seleccionProd) {
-                case 1 -> TipoProducto = Enumeracion.COCA_COLA;
-                case 2 -> TipoProducto = Enumeracion.SPRITE;
-                case 3 -> TipoProducto = Enumeracion.FANTA;
-                case 4 -> TipoProducto = Enumeracion.SNICKERS;
-                case 5 -> TipoProducto = Enumeracion.SUPER8;
+                case 1 -> productoElegido = TipoProducto.COCA_COLA;
+                case 2 -> productoElegido = TipoProducto.SPRITE;
+                case 3 -> productoElegido = TipoProducto.FANTA;
+                case 4 -> productoElegido = TipoProducto.SNICKERS;
+                case 5 -> productoElegido = TipoProducto.SUPER8;
             }
 
-            // 3. Intento de Compra con Manejo de Errores
+            /**
+             *  Intento de Compra con Manejo de Errores
+             */
             try {
-                Comprador cliente = new Comprador(monedaEntregada, TipoProducto, exp);[cite: 1]
-                System.out.println("¡Éxito! Consumiste: " + cliente.queConsumiste());[cite: 1]
+                Comprador cliente = new Comprador(monedaEntregada, productoElegido, exp);[cite: 1]
+                System.out.println("Consumiste: " + cliente.queConsumiste());[cite: 1]
                 System.out.println("Tu vuelto total es: $" + cliente.cuantoVuelto());[cite: 1]
             } catch (PagoIncorrectoException | PagoInsuficienteException | NoHayProductoException e) {
                 System.out.println("Error en la compra: " + e.getMessage());[cite: 1]
